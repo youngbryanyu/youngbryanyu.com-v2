d69daed3da3624d9eea96578bdada56bc932d252
Young Yu <youngyu19@gmail.com>
2024-05-31 14:47:43 -0700

    update hover

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -2,5 +2,5 @@
  * Linked text that is underlined and turns white or black when hovered over.
  */
 export function LinkedText(text: string, link: string) {
-    return (<a href={link} target='_blank' className="underline hover:text-black dark:hover:text-white">{text}</a>)
+    return (<a href={link} target='_blank' className="font-semibold hover:underline hover:text-black dark:hover:text-white">{text}</a>)
 }

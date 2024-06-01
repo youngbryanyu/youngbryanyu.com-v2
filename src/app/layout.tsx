68e09fcfc1a010753d42184e3345cbeab6fed45f
Young Yu <youngyu19@gmail.com>
2024-05-31 17:08:06 -0700

    change analytics from next  to react

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Providers } from '@/app/providers'
 import { Layout } from '@/components/Layout'
 
 import '@/styles/tailwind.css'
-import { Analytics } from '@vercel/analytics/next'
+import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 
 /**

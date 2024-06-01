70242cf248852e348873a5a0cdca2ef3b6ae7a7e
Young Yu <youngyu19@gmail.com>
2024-05-31 23:56:36 -0700

    update sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,4 +5,22 @@ module.exports = {
 
     /* Unused pages to exclude */
     exclude: ['/articles', '/speaking', '/thank-you', '/uses'],
+
+    /* Custom sitemap transform function */
+    transform: async (config, path) => {
+        /* Default priority for each page */
+        let priority = 0.5; 
+
+        /* Set priority to 1 for home page */
+        if (path === '/') {
+            priority = 1.0;
+        }
+
+        return {
+            loc: path, /* The URL of the page */
+            changefreq: 'daily', /* Change frequency */
+            priority, /* Priority */
+            lastmod: config.autoLastmod ? new Date().toISOString() : undefined, /* Last modified time */
+        }
+    },
 }

59fc1e22ec128c085ead1507f2559213702dd3fc
Young Yu <youngyu19@gmail.com>
2024-05-31 18:06:56 -0700

    create new social link hover effect

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -3,21 +3,30 @@ import Link from "next/link"
 import { ChevronRightIcon } from "./Icons"
 
 /**
- * A simple social link with just the icon (e.g. LinkedIn, GitHub, email, etc).
- * 
- * Highlights in zinc when hovered over. This looks better than teal for just an icon.
+ * A social link shown that is made up of a simple social link plus text below it when hovered over.
  */
-export function SimpleSocialLink({
+export function HoverSocialLink({
+    href,
+    ariaLabel,
     icon: Icon,
-    ...props
-}: React.ComponentPropsWithoutRef<typeof Link> & {
-    icon: React.ComponentType<{ className?: string }>
+    label,
+}: {
+    href: string;
+    ariaLabel: string;
+    icon: React.ComponentType<{ className?: string }>;
+    label: string;
 }) {
     return (
-        <Link className="group -m-1 p-1" {...props} target='_blank'>
-            <Icon className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
-        </Link>
-    )
+        <div className="relative flex flex-col items-center">
+            <div className="group flex flex-col items-center">
+                <SimpleSocialLink href={href} aria-label={ariaLabel} icon={Icon} />
+                <div className="absolute top-8 flex items-center opacity-0 transition-opacity duration-300 pointer-events-none group-hover:pointer-events-auto group-hover:opacity-100">
+                    <span className="ml-5 text-m text-zinc-600 dark:text-zinc-400">{label}</span>
+                    <ChevronRightIcon className="h-5 w-5 stroke-zinc-600 dark:stroke-zinc-400" />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 /**
@@ -51,28 +60,19 @@ export function VerboseSocialLink({
 }
 
 /**
- * A social link shown that is made up of a simple social link plus text below it when hovered over.
+ * A simple social link with just the icon (e.g. LinkedIn, GitHub, email, etc).
+ * 
+ * Highlights in zinc when hovered over. This looks better than teal for just an icon.
  */
-export function HoverSocialLink({
-    href,
-    ariaLabel,
+export function SimpleSocialLink({
     icon: Icon,
-    label,
-}: {
-    href: string;
-    ariaLabel: string;
-    icon: React.ComponentType<{ className?: string }>;
-    label: string;
+    ...props
+}: React.ComponentPropsWithoutRef<typeof Link> & {
+    icon: React.ComponentType<{ className?: string }>
 }) {
     return (
-        <div className="relative flex flex-col items-center">
-            <div className="group flex flex-col items-center">
-                <SimpleSocialLink href={href} aria-label={ariaLabel} icon={Icon} />
-                <div className="absolute top-8 flex items-center opacity-0 transition-opacity duration-300 pointer-events-none group-hover:pointer-events-auto group-hover:opacity-100">
-                    <span className="ml-5 text-m text-zinc-600 dark:text-zinc-400">{label}</span>
-                    <ChevronRightIcon className="h-5 w-5 stroke-zinc-600 dark:stroke-zinc-400" />
-                </div>
-            </div>
-        </div>
-    );
+        <Link className="group -m-1 p-1" {...props} target='_blank'>
+            <Icon className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
+        </Link>
+    )
 }
